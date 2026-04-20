6324a49c3061592727847f5e536a271bb2c38844
Anushri Awari <awarianushri@gmail.com>
2026-04-20 23:50:38 +0530

    Added TestController and cleaned project setup

---
+++ backend/InsuranceAPI/controller/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InsuranceAPI.controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("API is working");
+        }
+    }
+}
