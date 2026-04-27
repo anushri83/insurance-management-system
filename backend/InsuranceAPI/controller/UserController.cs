7205c67f62fc932074bea10016157ebed7c34111
Anushri Awari <awarianushri@gmail.com>
2026-04-27 23:59:00 +0530

    controller created for user

---
+++ backend/InsuranceAPI/controller/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsuranceAPI.controller
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
